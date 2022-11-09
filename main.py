1e1d67e89864fd99eb52abd6d04a50cb26d26a05
dylan <dylan.schertzer@outlook.fr>
2022-11-09 21:52:54 +0100

    conioeruoi

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 # projet staion essance 
-
+import random
 from csv import reader
 
-with open("voitures.csv", "r") as file:
-    reader = reader(file)
-    voiture = list(reader)
-
-with open("clients.csv", "r") as file:
-    reader = reader(file)
-    client = list(reader)
+with open("voitures.csv" , "r") as v ,open("clients.csv", "r") as c:
+    reader1 = reader(v)
+    reader2 = reader(c)
+    voitures = list(reader1)
+    clients = list(reader2)
 
 
-print(client[2][2])
-print(voiture[2][2])
+class waiting_line:
+    def __init__(self):
+        self.file = []
+        
+    def __str__(self):
+        txt = ""
+        for i in self.file:
+            txt += str(i) + " <- "
+        return txt
+    
+    def est_vide(self):
+        return self.file == []
+    
+    def enfiler(self, x):
+        self.file.append(x)
+
+    def defiler(self):
+        if self.est_vide():
+            raise MemoryError("File vide")
+        return self.file.pop(0)
+
+
+def random_voiture():
+    return random.choice(voitures)
+
+def random_client():
+    return random.choice(clients)
+
+print(random_voiture())
+print(random_client())
+
