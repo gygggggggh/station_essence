b97019d5412d6745f9f732e5ff6bb94bfb67d631
dylan <dylan.schertzer@outlooK.fr>
2022-11-17 16:15:04 +0100

    ok

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import random
 from csv import reader
 
-def ouvrir_csv(filename) -> list:
+def ouvrir_csv(filename) -> list: # dylan
     """Ouvre le fichier CSV nommé nom_fichier et renvoie une liste de listes"""
     try:
         with open(filename, "r",encoding='utf-8') as file:
@@ -23,7 +23,7 @@ clients = ouvrir_csv("clients.csv")
 
 
 
-class File:
+class File: # yanis
     ''' creation de la fonction class vue en cours 
      pour la gestion de la classe Pompes ''' 
 
@@ -49,20 +49,32 @@ class File:
             raise IndexError("La file est vide")
 
 
-class Pompe:
+class Pompe: # yanis 
     def __init__(self) -> None:
         self.pompe1 = File()
         self.pompe2 = File()
         self.pompe3 = File()
+        self.pompes = [self.pompe1, self.pompe2, self.pompe3]
+        self.remplir_pompe_debut()
     def random(self) -> int:
         voitures_random = voitures[random.randint(1, len(voitures)-1)]
         clients_random = clients[random.randint(1, len(clients)-1)]
         return [voitures_random, clients_random]
+
+    def remplir_pompe_debut(self) -> None:
+        self.pompes[0].enfiler(self.random())
+        self.pompes[1].enfiler(self.random())
+        self.pompes[2].enfiler(self.random())
+        return self.pompes
+
     def remplir_pompe(self) -> None:
-        self.pompe1.enfiler(self.random())
-        self.pompe2.enfiler(self.random())
-        self.pompe3.enfiler(self.random())
-        
+        return self.pompes[random.randint(0, 2)].enfiler(self.random())
+
+    def videz_pompe(self, pompe: int):
+        if self.pompes[pompe-1].est_vide():
+            return "la pompe est vide" 
+        else:
+            return self.pompes[pompe-1].defiler()
 
     def __str__(self) -> str:
         return f"pompe 1 : {self.pompe1} \npompe 2 : {self.pompe2} \npompe 3 : {self.pompe3}"
@@ -73,8 +85,8 @@ class Pompe:
     def covid(self, intensité: int):
         pass # a faire 
 
-d = Pompe()
-d.remplir_pompe()
-d.remplir_pompe()
-print(d)
-print(d.pompe_vide())
+if "__main__" == __name__:
+    pompe = Pompe()
+    pompe.videz_pompe(1)
+    print(pompe)
+    print(pompe.pompe_vide())
